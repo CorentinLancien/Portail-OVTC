2770483d10eb2f63e65e7058e871834b7efb3bc3
Corentin LANCIEN <CLANCI@CFCCI53.fr>
2020-03-13 14:04:15 +0100

    Add Page Animaux

--- Controllers/Front/AcceuilController.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-include_once $_SERVER['DOCUMENT_ROOT'] . '../Views/Front/acceuil.html';
--- public/Controllers/Front/AnimauxController.php
+++ public/Controllers/Front/AnimauxController.php
@@ -1,5 +1,5 @@
 <?php
 
 include_once $_SERVER['DOCUMENT_ROOT'] . 'Controllers/Structure/HeaderController.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . 'Views/Front/accueil.html';
+include_once $_SERVER['DOCUMENT_ROOT'] . 'Views/Front/animaux.html';
 include_once $_SERVER['DOCUMENT_ROOT'] . 'Controllers/Structure/FooterController.php';
