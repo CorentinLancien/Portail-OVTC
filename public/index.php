a6d1d8f26e9aa4fe99859f70d014eb425392b96e
Corentin LANCIEN <CLANCI@CFCCI53.fr>
2020-03-13 11:08:46 +0100

    add new architecture

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '../Controllers/Front/AcceuilController.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '../Controllers/Front/HeaderController.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '../Controllers/Structure/FooterController.php';
